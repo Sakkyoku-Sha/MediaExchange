49314f2a173727acf43d68b27fc073ef60213666
Sakkyoku-Sha <RyanAnderson0147@gmaiil.com>
2020-04-10 15:04:03 -0600

    Initial Setup of the Web App

---
+++ WebApplication/Web/react-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppHeader from "./Components/AppHeader";
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader userName={"Ryan Anderson"}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ WebApplication/Web/react-app/src/Components/AppHeader.tsx
@@ -0,0 +1,46 @@
+﻿import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import {makeStyles} from '@material-ui/core/styles'
+import {Button} from "@material-ui/core";
+
+const headerTheme = makeStyles({
+    header : {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        padding: '10px'
+    },
+
+    appBarDiv: {
+        position: 'relative',  
+    },
+    userInfo : {
+        position: 'relative',
+        width: '200px',
+        fontWeight : 'bold'
+    },
+    exchange : {
+        position: 'relative',
+        width: '200px',
+        row: 2,
+        fontWeight : 'bold',
+        overflow: 'auto',
+    }
+});
+
+interface headerProps {
+    userName : string
+}
+
+const AppHeader = (props : headerProps)  => {
+    
+    const styles = headerTheme();
+    
+    return <AppBar className={styles.header} position={"static"} color={"inherit"} variant={"elevation"}>
+            <div className={styles.appBarDiv}>
+                <Button className={styles.exchange}>Exchange</Button>
+                <Button className={styles.userInfo}>User Info</Button>
+            </div>
+        </AppBar>
+};
+
+
+export default AppHeader
